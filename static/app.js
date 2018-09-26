8a60c25ad43fee84dd4e684e41234225ecbf70dd
stefan safranek <stefan@stefan.stefan>
2018-09-26 12:37:01 -0700

    ui tabs for chatrooms

--- static/app.js
+++ static/app.js
@@ -1,107 +1,105 @@
 
-// strips out html...
-function sanitizeText(text) {
-    return $('<p>').html(text).text();
+function randomHash() {
+    return CryptoJS.MD5(
+                new Date().toISOString()
+            ).toString();
 }
 
-var chatApp = new Vue({
-    el: '#app',
+var app = {
+    "chatrooms": {}
+}
 
-    data: {
-        ws: null, // Our websocket
-        newMsg: '', // Holds new messages to be sent to the server
-        chatContent: '', // A running list of chat messages displayed on the screen
-        email: user.email, // Email address used for grabbing an avatar
-        username: user.username, // Our username
-        passphrase: null,
-        chatroom: null,
-        debug: true,
-        joined: false // True if email and username have been filled in
-    },
-
-    created: function() {},
-
-    methods: {
-
-        close: function(){
-            this.ws && this.ws.close();
-        },
-
-        send: function () {
-            if (this.newMsg != '') {
-
-                var data = JSON.stringify({
-                    email: this.email,
-                    username: this.username,
-                    message: sanitizeText(this.newMsg)
-                });
-
-                var encrypted = CryptoJS.AES.encrypt(data, this.passphrase);
-                var payload = encrypted.toString();
-
-                this.ws.send(
-                    JSON.stringify({"data": payload})
-                );
-                this.newMsg = ''; // Reset newMsg
+$('.newChatRoom').on('click', function(){
+    swal({
+        title: 'Join Chatroom',
+        html: '<input id="chatroom_name" class="swal2-input" placeholder="chatroom">' +
+            '<input id="chatroom_passphrase" class="swal2-input" placeholder="passphrase">',
+        showCancelButton: true,
+        focusConfirm: false,
+        preConfirm: () => {
+            return {
+                chatroom: document.getElementById('chatroom_name').value,
+                passphrase: document.getElementById('chatroom_passphrase').value
             }
-        },
+        }
+    }).then(function(value){
+        if (value) {
+            var result = value.value;
 
-        join: function () {
-            if (!this.chatroom) {
-                Materialize.toast('You must choose a chat room', 2000);
+            if (app.chatrooms[result.chatroom]) {
+                M.toast({html: 'Chatroom already open', displayLength: 3000});
                 return;
             }
-            this.passphrase = sanitizeText(this.passphrase);
-            this.chatroom = sanitizeText(this.chatroom);
-            this.joined = true;
-            this.connect();
-        },
 
-        connect: function(){
-            var self = this;
-
-            // $('.chatroom .card-title span').text(this.chatroom);
-            // HACK
-            setTimeout(function(){  $('.chatroom .card-title span').text(self.chatroom); },250);
-            //.end
-
-            this.ws = new WebSocket('ws://' + window.location.host + '/ws/' + this.chatroom);
-
-            this.ws.addEventListener('message', function(e) {
-                console.log(e.data);
-                try {
-                    var result = JSON.parse(e.data);
-                    var decrypted = CryptoJS.AES.decrypt(result.data, self.passphrase);
-                    var msg = decrypted.toString(CryptoJS.enc.Utf8);
-                    msg = JSON.parse(msg);
-                    // write message to page
-                    self.chatContent += '<div class="chip">'
-                            + '<img src="' + self.gravatarURL(msg.email) + '">' // Avatar
-                            + msg.username
-                        + '</div>'
-                        + emojione.toImage(msg.message) + '<br/>'; // Parse emojis
-                    var element = document.getElementById('chat-messages');
-                    element.scrollTop = element.scrollHeight; // Auto scroll to the bottom
-                }
-                catch(err) {
-                    Materialize.toast('Unable to parse incomming message', 3000);
-                    return;
-                }
+            app.chatrooms[result.chatroom] = new ChatRoom(result.chatroom, result.passphrase, function() {
+                delete app.chatrooms[result.chatroom];
+                removeTab(result.chatroom);
             });
 
-            this.ws.onopen = function(e) {
-                Materialize.toast('Connection open', 2000);
-            }
-
-            this.ws.onclose = function(e) {
-                Materialize.toast('Connection closed', 15000);
-                self.joined = false;
-            }
-
-        },
-
-        gravatarURL: function(email) {
-            return 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(email);
+            addTab(result.chatroom).append(
+                app.chatrooms[result.chatroom].$el
+            );
         }
-    }
+    });
 });
+
+
+
+
+function buildTabs() {
+    var $tabs = $('.tabs');
+    var instance = M.Tabs.getInstance($tabs);
+    instance && instance.destroy();
+    $tabs.children().length && M.Tabs.init($tabs, {});
+}
+
+function removeTab(tab_name) {
+    if (!tab_name) {
+        M.toast({html: 'Must supply a tab name', displayLength: 3000});
+        return;
+    }
+    var tab_id = CryptoJS.MD5(tab_name).toString();
+    $('#'+tab_id).remove();
+    buildTabs();
+}
+
+function addTab(tab_name) {
+    if (!tab_name) {
+        M.toast({html: 'Must supply a tab name', displayLength: 3000});
+        return;
+    }
+
+    var tab_id = CryptoJS.MD5(tab_name).toString();
+
+    // make singleton
+    removeTab(tab_name);
+
+    var $tabLabel = $('<li>').addClass('tab').append(
+        $('<a>', {
+            href: '#'+tab_id
+        }).text(tab_name)
+    );
+
+    $('#chatrooms .tabs').append( $tabLabel );
+
+    var $content = $('<div>', {id: tab_id})
+                        .addClass('col s12')
+                        .on('remove', function(){
+                            $tabLabel.remove();
+                        });
+
+    $('#chatrooms').append(
+        $content
+    );
+
+    // rebuild tabs
+    buildTabs();
+
+    // select new tab
+    var $tabs = $('.tabs');
+    var instance = M.Tabs.getInstance($tabs);
+    instance.select(tab_id);
+
+    // return tab content
+    return $content;
+}
--- static/chatroom.js
+++ static/chatroom.js
@@ -8,7 +8,7 @@ function gravatarURL(email) {
     return 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(email);
 }
 
-var ChatRoom = function(chatroom, passphrase){
+var ChatRoom = function(chatroom, passphrase, onClose){
     this.ws = null;
     this.data = {
         email: user.email,
@@ -16,6 +16,7 @@ var ChatRoom = function(chatroom, passphrase){
         passphrase: passphrase,
         chatroom: chatroom
     }
+    this.onClose = onClose;
     this.open();
     this.buildUi();
 }
@@ -26,7 +27,6 @@ ChatRoom.prototype.open = function() {
     this.ws = new WebSocket('ws://' + window.location.host + '/ws/' + this.data.chatroom);
 
     this.ws.addEventListener('message', function(e) {
-        console.log(e.data);
         try {
             var result = JSON.parse(e.data);
             var decrypted = CryptoJS.AES.decrypt(result.data, self.data.passphrase);
@@ -38,23 +38,25 @@ ChatRoom.prototype.open = function() {
                     + '<img src="' + gravatarURL(msg.email) + '">' // Avatar
                     + msg.username
                 + '</div>'
+                // Parse emojis
                 + emojione.toImage(msg.message) + '<br/>'
-            ); // Parse emojis
-            var element = document.getElementById('chat-messages');
-            element.scrollTop = element.scrollHeight; // Auto scroll to the bottom
+            );
+            // Auto scroll to the bottom
+            self.chatMessages.scrollTop = self.chatMessages.scrollHeight;
         }
         catch(err) {
-            Materialize.toast('Unable to parse incomming message', 3000);
+            console.log(err)
+            M.toast({html: 'Unable to parse incomming message', displayLength: 3000});
             return;
         }
     });
 
     this.ws.onopen = function(e) {
-        Materialize.toast('Connection open', 2000);
+        M.toast({html: 'Connection open', displayLength: 2000});
     }
 
     this.ws.onclose = function(e) {
-        Materialize.toast('Connection closed', 15000);
+        M.toast({html: 'Connection closed', displayLength: 5000});
     }
 }
 
@@ -65,6 +67,7 @@ ChatRoom.prototype.destroy = function() {
 ChatRoom.prototype.close = function() {
     this.ws && this.ws.close();
     this.destroy();
+    this.onClose && this.onClose();
 }
 
 ChatRoom.prototype.send = function() {
@@ -96,7 +99,8 @@ ChatRoom.prototype.buildUi = function() {
     this.inputMessage = $('<input>', {type: 'text', id:'message'}).addClass('validate');
 
     this.$el = $('<div>').addClass('row').append(
-        $('<div>').addClass('col s12 m6').append(
+        // $('<div>').addClass('col s12 m6').append(
+        $('<div>').addClass('col s12').append(
             $('<div>').addClass('card').append(
                 $('<div>').addClass('card-title').append(
                     $('<span>').append(this.data.chatroom),
@@ -129,9 +133,3 @@ ChatRoom.prototype.buildUi = function() {
         )
     );
 }
-
-
-
-var room = new ChatRoom('test', '1234');
-
-$('main').append(room.$el);
